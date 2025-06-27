e83b5263cc47c2b5f174669ae0f64609b0567201
zeyadgebril <zeyadgebril176@gmail.com>
2025-06-27 18:55:16 +0300

    update Repository Standerd

---
+++ Warehouse Management System/Repository/ProductRepositoryFile/IProductRepository.cs
@@ -0,0 +1,9 @@
+﻿using Warehouse_Management_System.Models;
+using Warehouse_Management_System.Models;
+namespace Warehouse_Management_System.Repository.Product
+{
+    public interface IProductRepository:IRepository<Product> 
+    {
+
+    }
+}
--- Warehouse Management System/Repository/UnitOfWork.cs
+++ Warehouse Management System/Repository/UnitOfWork.cs
@@ -1,6 +1,27 @@
-﻿namespace Warehouse_Management_System.Repository
+﻿using Microsoft.EntityFrameworkCore;
+using Warehouse_Management_System.Repository.ProductRepositoryFile;
+
+namespace Warehouse_Management_System.Repository
 {
     public class UnitOfWork
     {
-    }
+        private readonly DbContext db;
+        private IProductRepository _productRepository;
+
+        public UnitOfWork(DbContext db)
+        {
+            this.db = db;
+        }
+
+        public IProductRepository ProductRepository
+        {
+			get 
+			{
+				if (_productRepository == null)
+					_productRepository = new ProductRepository(db);
+				return _productRepository; 
+			}
+		}
+
+	}
 }
