3432b75ad0eedb22e72a23710e6c57599d18ae1b
Zeyad Gebril <67805019+zeyadgebril@users.noreply.github.com>
2025-06-28 12:29:40 +0300

    111

---
+++ Warehouse Management System/CustomAttribute/UniqueNameAttribute.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.Extensions.DependencyInjection;
+using Warehouse_Management_System.Models;
+
+namespace Warehouse_Management_System.CustomAttribute
+{
+    public class UniqueNameAttribute : ValidationAttribute
+    {
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            var db = validationContext.GetService<dbContext>();
+
+            if (db == null)
+            {
+                throw new Exception("Database in CustomAttribute is not available in ValidationContext.");
+            }
+
+            var name = value?.ToString();
+
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                return new ValidationResult("Name cannot be empty.");
+            }
+
+            var existingName = db.Users.FirstOrDefault(e => e.UserName == name);
+
+            if (existingName == null)
+            {
+                return ValidationResult.Success;
+            }
+
+            return new ValidationResult("This username is already taken.");
+        }
+    }
+}
---
+++ Warehouse Management System/MapperConfig/mapperConfig.cs
@@ -0,0 +1,9 @@
+﻿using AutoMapper;
+
+namespace Warehouse_Management_System.MapperConfig
+{
+    public class mapperConfig:Profile
+    {
+        //Implement the mapping config
+    }
+}
